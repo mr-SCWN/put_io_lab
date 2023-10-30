7b2c3fe2a5410a7b2b9a125a98cb725916d1ed55
ivankaliadzich <2004ivanko@gmail.com>
2023-10-30 16:48:02 +0100

    adding numbers for fibonacci and printing 0 and 1

--- 04-git/Program.java
+++ 04-git/Program.java
@@ -1,5 +1,10 @@
 class programjava{
     public static void main(String[] args) {
         System.out.println("Hello, World and Politechnika!"); 
+
+
+	 int n1=0,n2=1,n3,i,count=10;    
+ 	System.out.println(n1+" "+n2);//printing 0 and 1    
+    
     }
 }
