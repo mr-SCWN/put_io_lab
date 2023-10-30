e9659cc7afe6c484a2e13b53c81104a3adf17f86
ivankaliadzich <2004ivanko@gmail.com>
2023-10-30 17:34:16 +0100

    retrying zadanie 5

--- 04-git/Program.java
+++ 04-git/Program.java
@@ -1,20 +1,12 @@
-class programjava{ // program prints strings and Fibonacci Series
+class HelloWorld{ 
     public static void main(String[] args) {
   
-	 System.out.println("Hello World!"); 	
-
-        System.out.println("Hello Politechnika!"); 
-
+	 System.out.println("Hello World and Politechnika!"); 	
 
 	int n1=0,n2=1,n3,i,count=10;    
- 	System.out.print(n1+" "+n2+" ");//printing 0 and 1    
-    	
-	 for(i=2;i<count;++i)//loop starts from 2 because 0 and 1 are already printed    
- {    
-  n3=n1+n2;    
-  System.out.print(" "+n3);    
-  n1=n2;    
-  n2=n3;    
- }  
+ 	System.out.print(n1+" ");//printing 0 
+
+	System.out.print(n2+" ");//printing 1     	
+	 
     }
 }
