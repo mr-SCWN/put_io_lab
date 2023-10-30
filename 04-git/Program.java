a7d4265c83690749871e515e4d592ff7c29816d1
ivankaliadzich <2004ivanko@gmail.com>
2023-10-30 17:35:47 +0100

    changing 1st fragment
    changing 2nd fragment

--- 04-git/Program.java
+++ 04-git/Program.java
@@ -1,7 +1,9 @@
-class HelloWorld{ 
+class programjava{
     public static void main(String[] args) {
   
-	 System.out.println("Hello World and Politechnika!"); 	
+	 System.out.println("Hello World!"); 	
+
+        System.out.println("Hello Politechnika!"); 
 
 	int n1=0,n2=1,n3,i,count=10;    
  	System.out.print(n1+" ");//printing 0 
